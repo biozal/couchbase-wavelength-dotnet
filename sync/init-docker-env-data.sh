029f94af1507e2b7665911a4f4329fc19284f2b0
Aaron LaBeau <alabeau@gmail.com>
2021-11-01 16:33:17 -0400

    updated for creating indexes and auto-setting up server

--- sync/init-docker-env-data.sh
+++ sync/init-docker-env-data.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+docker exec -it cb-server-7 couchbase-cli cluster-init -c 127.0.0.1 \
+--cluster-username Administrator \
+--cluster-password password \
+--services data,index,query \
+--cluster-ramsize 1024 \
+--cluster-index-ramsize 256 
+
+sleep(5)
 curl -v -X POST http://localhost:8091/pools/default/buckets \
 -u Administrator:password \
 -d name=wavelength \
@@ -7,7 +16,7 @@ curl -v -X POST http://localhost:8091/pools/default/buckets \
 sleep(10)
 curl -v http://localhost:8093/query/service \
   -u Administrator:password \
-  -d 'statement=CREATE INDEX idx_wavelength_document_type on wavelength(type)'
+  -d 'statement=CREATE INDEX idx_wavelength_document_type on wavelength(documentType)'
 
 sleep(5)
 curl -v http://localhost:8093/query/service \
@@ -17,6 +26,6 @@ curl -v http://localhost:8093/query/service \
 docker cp sample-documents-server.json \
 cb-server-7:/sample-documents-server.json
 
-docker exec -it cb-server /opt/couchbase/bin/cbimport json --format list \
+docker exec -it cb-server-7 /opt/couchbase/bin/cbimport json --format list \
   -c http://localhost:8091 -u Administrator -p password \
   -d "file:///sample-documents-server.json" -b 'wavelength' -g %documentId%
