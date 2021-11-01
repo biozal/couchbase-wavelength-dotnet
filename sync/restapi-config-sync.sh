e7b1dae3985c9ce345ee8dccdf10c8567dc22aa0
Aaron LaBeau <alabeau@gmail.com>
2021-11-01 12:41:36 -0400

    updated config files

--- sync/restapi-config-sync.sh
+++ sync/restapi-config-sync.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-curl --location --request PUT 'http://127.0.0.1:4985/wavelengthdb/' \  
---header 'Authorization: Basic c3luY19nYXRld2F5OnBhc3N3b3Jk' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "bucket": "wavelength", 
-    "num_index_replicas": 0 
-    }'
+curl -v -X PUT http://127.0.0.1:4985/wavelengthdb/ \  
+-H 'Authorization: Basic c3luY19nYXRld2F5OnBhc3N3b3Jk' \
+-H 'Content-Type: application/json' \
+-d '{ "bucket": "wavelength", "num_index_replicas": 0 }'
